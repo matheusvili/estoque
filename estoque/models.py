f6feae67ebf7d88a3be8f504d0d87f1ea1d37686
MARIA  ROSA <ROSAM25@sa.ad.whirlpool.com>
2025-05-20 09:37:38 -0300

    model_categoria_produto

---
+++ estoque/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+def __str__(self):
+    return self.descricao
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
