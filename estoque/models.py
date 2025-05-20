e4f8bd0b86a9e960bd702aef3652a4720f3b5170
matheusvili <viliczinskimj@gmail.com>
2025-05-20 11:08:11 -0300

    model produto arrumado mais ou menos

--- estoque/models.py
+++ estoque/models.py
@@ -9,7 +9,13 @@ def __str__(self):
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True)
+    quantidade = models.CharField()
+    numero_serie = models.CharField(max_length=100)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, default=1)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_cadastro = models.DateField()
+
+
 
     def __str__(self):
         return self.nome
