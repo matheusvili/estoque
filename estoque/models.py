d9b5d0e65a81c6f88f4bcffb3a4e2cf5a6dc22d1
matheusvili <viliczinskimj@gmail.com>
2025-05-20 10:41:16 -0300

    model_usuario

--- estoque/models.py
+++ estoque/models.py
@@ -13,3 +13,14 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    RE = models.CharField(max_length=10)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
